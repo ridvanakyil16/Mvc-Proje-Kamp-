014f0a69800bec9ca828b288097d0f6d7f66231b
Rıdvan <ridvanakyil16@gmail.com>
2021-07-24 19:54:54 +0300

    Mvc Projelerim Bunlar
    Murat YÜCEDAĞ ile birlikte başladığım bu projeninin geldiğim kadarki yeri

---
+++ MvcSitesi/DataAccsessLayer/Abstract/IRepositoryDal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccsessLayer.Abstract
+{
+    public interface IRepositoryDal<T>
+    {
+        //CRUD
+        //Type Name();
+
+        List<T> List();
+
+        void Insert(T p);
+        void Delete(T p);
+        void Update(T P);
+
+        List<T> List(Expression<Func<T, bool>> filter);
+
+    }
+}
